f249f4ac6513b4ae63fc366e7cdf79a468beceb5
dr1co <dricomatoba@hotmail.com>
2024-07-16 10:26:14 -0300

    [fix] added cors configuration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,17 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import cors from "cors";
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get("/example", (req: Request, res: Response) => {
+  res.send("Hello World!");
+})
 
 app.use(express.json());
 app.use(cors());
